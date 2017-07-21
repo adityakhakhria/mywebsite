5b74a93946b3be278d96d69a8a09e04c14d5257b
Adityatechnology <adkhakhs0412@gmail.com>
2017-07-21 21:32:58 +0530

    Dribbble added

---
+++ dribbble/config.js
@@ -0,0 +1,10 @@
+// Howdy! This is your website's configuration file.
+// Just replace my personal info with yours, save, and you're done :)
+
+dribbbox.config = {
+
+  dribbble_username : "aditya12",
+  short_description : "Web / User interface and Graphic designer.",
+  email_address     : "adkhakhs0412@gmail.com"
+
+}
